2d500a0f8135acf1ec0a287c013ca2e7e190ea2c
amalthomas1996 <thomasamal61@gmail.com>
2024-05-18 20:56:05 +0530

    last commit

---
+++ src/Components/VideoPayer/VideoPayer.jsx
@@ -0,0 +1,24 @@
+import React, { useRef } from "react";
+import "./VideoPayer.css";
+import video from "../../assets/college_video.mp4";
+
+const VideoPayer = ({ playState, setPlayState }) => {
+  const player = useRef(null);
+  const closePlayer = (e) => {
+    if (e.target === player.current) {
+      setPlayState(false);
+    }
+  };
+
+  return (
+    <div
+      className={`videopayer ${playState ? "" : "hide"}`}
+      ref={player}
+      onClick={closePlayer}
+    >
+      <video src={video} autoPlay muted controls></video>
+    </div>
+  );
+};
+
+export default VideoPayer;
